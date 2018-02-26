2dd983f5efd67de0126b003c085d2b768a3675b4
laravel <lucasp18@gmail.com>
2018-02-26 00:11:50 -0300

    add novo endpoint aplicacoes

--- api/index.php
+++ api/index.php
@@ -12,5 +12,6 @@ require '_genesis/genesis.php';
 require 'endpoints/usuarios.php';
 require 'endpoints/animais.php';
 require 'endpoints/proprietarios.php';
+require 'endpoints/aplicacoes.php';
 
 $app->run();
